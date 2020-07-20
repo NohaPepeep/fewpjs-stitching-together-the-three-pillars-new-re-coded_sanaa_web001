6aa2c9bfd16604e9a237b6c505b3b6aa9d437fd5
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-07-20 02:49:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,25 +36,12 @@ function likeCallback(e) {
     });
 }
 
-// STEP 3: In order for the call to the server and the update of the screen to
-// work, the elements we identify in STEP 1 need to be told to run that update
-// code when an "event" is fired. That's Pillar 2, event handling. Uncomment
-// this code.
 
 //for (let glyph of articleHearts) {
   //glyph.addEventListener("click", likeCallback);
 //}
 
-// STEP 4: 
-
-// When all the STEPs' code changes have been complete, you'll be able to see a
-// working demonstration of our reference example. Sure, it's maybe not as
-// pretty as a professional site, but they're only different from our site in
-// style, not substance.
-
-//------------------------------------------------------------------------------
-// Ignore after this point. Used only for demo purposes
-//------------------------------------------------------------------------------
+-
 
 function mimicServerCall() {
   return new Promise(function(resolve, reject) {
